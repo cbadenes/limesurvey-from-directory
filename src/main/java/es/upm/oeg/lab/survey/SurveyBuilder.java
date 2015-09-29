edf554bc45dcdb08689ac3096d17b00bbb7213f4
Carlos Badenes <cbadenes@gmail.com>
2015-09-29 14:05:06 +0200

    surveys generated for local and remote environment

--- src/main/java/es/upm/oeg/lab/survey/SurveyBuilder.java
+++ src/main/java/es/upm/oeg/lab/survey/SurveyBuilder.java
@@ -20,8 +20,15 @@ public class SurveyBuilder {
                         "/Users/cbadenes/Documents/OEG/Projects/DrInventor/datasets/acm-siggraph-2006-2014/papers",
                         Lists.newArrayList("Modeling", "Stereo", "Game", "Animation", "Rendering", "Image", "Perception", "Hardware", "Light", "3DPrinting", "Display"),
                         "192.168.59.103:8080",
-                        "siggraph-100.lss");
+                        "siggraph-100-local.lss");
 
+        limeSurvey.write
+                ("SigGraph Dataset",
+                        "SIGGRAPH (short for Special Interest Group on GRAPHics and Interactive Techniques) is the name of the annual conference on computer graphics (CG) convened by the ACM SIGGRAPH organization",
+                        "/Users/cbadenes/Documents/OEG/Projects/DrInventor/datasets/acm-siggraph-2006-2014/papers",
+                        Lists.newArrayList("Modeling", "Stereo", "Game", "Animation", "Rendering", "Image", "Perception", "Hardware", "Light", "3DPrinting", "Display"),
+                        "zavijava.dia.fi.upm.es:9191",
+                        "siggraph-100-zavijava.lss");
 
         limeSurvey.write
                 ("SigGraph Dataset",
@@ -29,7 +36,15 @@ public class SurveyBuilder {
                         "/Users/cbadenes/Documents/OEG/Projects/DrInventor/datasets/acm-siggraph-2006-2014/2-papers",
                         Lists.newArrayList("Modeling", "Stereo", "Game", "Animation", "Rendering", "Image", "Perception", "Hardware", "Light", "3DPrinting", "Display"),
                         "192.168.59.103:8080",
-                        "siggraph-2.lss");
+                        "siggraph-2-local.lss");
+
+        limeSurvey.write
+                ("SigGraph Dataset",
+                        "SIGGRAPH (short for Special Interest Group on GRAPHics and Interactive Techniques) is the name of the annual conference on computer graphics (CG) convened by the ACM SIGGRAPH organization",
+                        "/Users/cbadenes/Documents/OEG/Projects/DrInventor/datasets/acm-siggraph-2006-2014/2-papers",
+                        Lists.newArrayList("Modeling", "Stereo", "Game", "Animation", "Rendering", "Image", "Perception", "Hardware", "Light", "3DPrinting", "Display"),
+                        "zavijava.dia.fi.upm.es:9191",
+                        "siggraph-2-zavijava.lss");
 
         limeSurvey.write
                 ("ISWC 2015 Dataset",
@@ -37,7 +52,17 @@ public class SurveyBuilder {
                         "/Users/cbadenes/Documents/OEG/Projects/DrInventor/datasets/iswc-2015/papers",
                         Lists.newArrayList("Research", "Empirical_Studies_and_Experiments", "In-Use_and_Software", "Data_Sets_and_Ontologies"),
                         "192.168.59.103:8080",
-                        "iswc.lss");
+                        "iswc-local.lss");
+
+        limeSurvey.write
+                ("ISWC 2015 Dataset",
+                        "Papers from The 14th International Semantic Web Conference",
+                        "/Users/cbadenes/Documents/OEG/Projects/DrInventor/datasets/iswc-2015/papers",
+                        Lists.newArrayList("Research", "Empirical_Studies_and_Experiments", "In-Use_and_Software", "Data_Sets_and_Ontologies"),
+                        "zavijava.dia.fi.upm.es:9191",
+                        "iswc-zavijava.lss");
+
+        //http://zavijava.dia.fi.upm.es:9191
 
 
     }
